05a386644df31978fcd46234500835828e4ce4ce
AsgardPavlov <pasgard9@gmail.com>
2022-08-11 00:02:11 +0600

    fix webpack.common

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -12,6 +12,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, '..', './build'),
         filename: 'bundle.js',
+        publicPath: '/',
     },
     plugins: [
         new CopyWebpackPlugin({
